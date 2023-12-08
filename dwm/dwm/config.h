8ea483bebf2390b3214e39faa297410242e71e27
sunorcio <santi.mosvil@gmail.com>
2023-12-08 01:38:30 +0100

    asdf

--- dwm/dwm/config.h
+++ dwm/dwm/config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 	{ Mod1Mask|ShiftMask,			KEY,      toggletag,      {.ui = 1 << TAG} }, */
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define TERMINAL "alacritty"
+#define TERMINAL "st"
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 #define TSHCMD(tittle,cmd) { .v = (const char*[]){ "/bin/sh", "-c",TERMINAL " -T " tittle " -e " cmd, NULL } }
 /* commands */
